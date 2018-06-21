edb2ba047c38241442cb662b59eabed245a6910c
Anuj Shrivastava <anuj@gaurish.com>
2018-06-21 15:17:54 +0530

    Resolved PMD warnings

--- src/ClassPdo.php
+++ src/ClassPdo.php
@@ -2,14 +2,14 @@
 
 	class ClassPdo
 	{
-		public $cn;
+		public $conection;
 		
 		public function ClassPdo()
 		{
 			try
 			{
-				$this->cn=new PDO("mysql:host=localhost; dbname=student","root","");
-				return $this->cn;
+				$this->conection=new PDO("mysql:host=localhost; dbname=student","root","");
+				return $this->conection;
 			}
 			catch(PDOException $e)
 			{
@@ -21,16 +21,16 @@
 		public function show()
 		{
 			$sql="SELECT * FROM registration";
-			$stmt=$this->cn->query($sql);
+			$stmt=$this->conection->query($sql);
 			return $stmt->fetchAll(PDO::FETCH_OBJ);
 		}
 		
 		//showSpecific($id) is used to fetch single record in the database table by id.
-		public function showSpecific($id)
+		public function showSpecific($student_id)
 		{
 			$sql="SELECT * FROM registration WHERE id=?";
-			$stmt=$this->cn->prepare($sql);
-			$stmt->execute([$id]);
+			$stmt=$this->conection->prepare($sql);
+			$stmt->execute([$student_id]);
 			
 			if($stmt)
 			{
@@ -39,51 +39,47 @@
 		}
 		
 		//checkEmail($email) is used to check email is exist or not in database at the time of update
-		public function checkEmailUpdate($email,$id)
+		public function checkEmailUpdate($email,$student_id)
 		{
+			$result='false';
 			$sql="SELECT email FROM registration WHERE id!=:id && email=:email";
-			$stmt=$this->cn->prepare($sql);
-			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+			$stmt=$this->conection->prepare($sql);
+			$stmt->bindParam(':id', $student_id, PDO::PARAM_INT);
 			$stmt->bindParam(':email', $email, PDO::PARAM_STR);
 			$stmt->execute();
 			//var_dump($stmt);
 			//echo '<br>'.$stmt->rowCount();
 			if($stmt->rowCount()>0)  // rowCount() is used to count the no. of column is fetched. 
 			{
-				echo "true";		
+				$result='true';				
 			}
-			else
-			{
-				echo "false";
-			}
+			echo $result;
 			
 		}
 		
 		//checkEmail($email) is used to check email is exist or not in database at the time of submission
 		public function checkEmail($email)
 		{
+			$result='false';
 			$sql="SELECT email FROM registration WHERE email=?";
-			$stmt=$this->cn->prepare($sql);
+			$stmt=$this->conection->prepare($sql);
 			$stmt->execute([$email]);
 			//var_dump($stmt);
 			if($stmt->rowCount()>0) // rowCount() is used to count the no. of column is fetched. 
 			{
-				echo "true";		
+				$result="true";		
 			}
-			else
-			{
-				echo "false";
-			}
+			echo $result;
 			
 		}
 		
 		//update(all extract parameter) is used to update particular record in database table.
-		public function update($id,$fname,$lname,$date,$password,$cpassword,$email,$city,$address,$gender,$hobby)
+		public function update($student_id,$fname,$lname,$date,$password,$cpassword,$email,$city,$address,$gender,$hobby)
 		{
 			//query of update it return true(1) when record is update and false(0) when record is not update.
 			$sql="UPDATE registration SET first_name=?, last_name=?, dob=?, pwd=?, confirm_pwd=?, email=?, city=?, address=?, gender=?, hobby=? WHERE id=? ";
-			$stmt=$this->cn->prepare($sql);
-			$stmt->execute([$fname,$lname,$date,$password,$cpassword,$email,$city,$address,$gender,$hobby,$id]);
+			$stmt=$this->conection->prepare($sql);
+			$stmt->execute([$fname,$lname,$date,$password,$cpassword,$email,$city,$address,$gender,$hobby,$student_id]);
 			return $stmt;
 			
 		}
@@ -93,23 +89,20 @@
 		{
 			//query of insert it return true(1) when record is inserted and false(0) when record is not inserted.
 			$sql="insert into registration (first_name, last_name, dob, pwd, confirm_pwd, email, city, address, gender, hobby) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-			$stmt=$this->cn->prepare($sql);
+			$stmt=$this->conection->prepare($sql);
 			$stmt->execute([$fname,$lname,$date,$password,$cpassword,$email,$city,$address,$gender,$hobby]);
 			if($stmt)
 			{
 				return true;
 			}
-			else
-			{
-				return false;
-			}
+			return false;
 		}
 		
 		//delete($email) is used to delete a record in database table by email id
 		public function delete($email)
 		{
 			$sql="DELETE FROM registration WHERE email=?";
-			$stmt=$this->cn->prepare($sql);
+			$stmt=$this->conection->prepare($sql);
 			$stmt->execute([$email]);
 			$count=$stmt->rowCount();
 			//var_dump($stmt);
